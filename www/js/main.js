5a920016185011aabda2f517090ac143cfec7244
TrinityCarnegie <tcarnegie2017@fau.edu>
2021-12-05 16:22:52 -0500

    Homepage Tab

---
+++ www/js/main.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //This is the DOM ready wrapper, this is basically the entry point for the JS once the dom content is loaded.
+
+    //Set event listeners/handlers for buttons
+    document.getElementById('home').onclick = dohome;
+    document.getElementById('recipesearch').onclick = dorecipesearch;
+    document.getElementById('favorites').onclick = dofavorites;
+    document.getElementById('signin').onclick = dosignin;
+    
+    //Set listeners for Auth State Changed
+    firebase.auth().onAuthStateChanged(function (user) {
+        //if there is a user enable app functionality
+        if (user) {
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            buttons_signed_in();
+            document.getElementById('signintext').innerHTML = "Sign-out"
+            document.getElementById('signin').onclick = function (){
+                firebase.auth().signOut();
+
+            }
+
+            //else keep the app disabled or re-disabled it
+        } else {
+            buttons_not_signed_in();
+            document.getElementById('signin').onclick = dosignin;
+            document.getElementById('signintext').innerHTML = "Sign-In"
+
+
+        }
+    });
+    //Make sure the user is logged out and not a saved session
+    firebase.auth().signOut();
+
+    //Disable the buttons
+    buttons_not_signed_in()
+
+    dohome();
+});
+
+//Helper functions for common tasks
+
+
+//Disable all the buttons since we arent signed in
+function buttons_not_signed_in(){
+    document.getElementById('home').disabled = false;
+    document.getElementById('recipesearch').disabled = true;
+    document.getElementById('favorites').disabled = false;
+    document.getElementById('signin').disabled = false;
+}
+
+
+//Enable the buttons since we're signed in now.
+function buttons_signed_in(){
+    document.getElementById('home').disabled = false;
+    document.getElementById('recipesearch').disabled = false;
+    document.getElementById('favorites').disabled = false;
+    document.getElementById('signin').disabled = false;
+}
+
+//unselect all buttons
+function clean_buttons(){
+    document.getElementById('home').classList.remove("selected")
+    document.getElementById('recipesearch').classList.remove("selected")
+    document.getElementById('favorites').classList.remove("selected")
+    document.getElementById('signin').classList.remove("selected")
+
+}
