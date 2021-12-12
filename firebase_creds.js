389b5fa88925cdf9d9415f78537166756cb29830
TrinityCarnegie <94204441+TrinityCarnegie@users.noreply.github.com>
2021-12-12 14:54:02 -0500

    Google sign-in added + updated Nav Menu

---
+++ firebase_creds.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCoA_B5fjrIrp_K-y_SnTk66osuQUHD6j0",
+    authDomain: "the-happy-hour.firebaseapp.com",
+    projectId: "the-happy-hour",
+    storageBucket: "the-happy-hour.appspot.com",
+    messagingSenderId: "486485192561",
+    appId: "1:486485192561:web:ff87e4f673deb053ed50d0",
+    measurementId: "G-3RQ06VXWNL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ home.js
@@ -0,0 +1,22 @@
+var dohome = function(){
+
+    clean_buttons();
+    document.getElementById('home').classList.add("selected");
+    var workspace = document.getElementById("content");
+    workspace.innerHTML = "";
+
+    var h1 = document.createElement('h1');
+    var text = document.createTextNode("The Happy Hour");
+    h1.appendChild(text);
+    workspace.append(h1);
+
+    var p1 = document.createElement('p1');
+    var text = document.createTextNode("Because it's five o'clock somewhere...");
+    p1.appendChild(text);
+    workspace.append(p1);
+
+    var h3 = document.createElement('h3');
+    var txt = document.createTextNode("Trinity Carnegie : Z23432125");
+    h3.appendChild(txt);
+    workspace.append(h3);
+}
